f8b74bf4a79f9452fab52492a9881bf14c0db2f2
caroolcanelas <carolina.canelas@soulasalle.com>
2025-01-20 16:02:43 -0300

    testando
    Signed-off-by: caroolcanelas <carolina.canelas@soulasalle.com>

--- aplicador_de_prova/src/aplicador_de_prova/TeseApp.java
+++ aplicador_de_prova/src/aplicador_de_prova/TeseApp.java
@@ -1,5 +1,9 @@
 package aplicador_de_prova;
 
 public class TeseApp {
+	public static void main(String[]args) {
+		System.out.println("teste");
+
+	}
 
 }
---
+++ aplicador_prova/src/aplicador_prova/TesteApp.java
@@ -0,0 +1,5 @@
+package aplicador_prova;
+
+public class TesteApp {
+
+}
